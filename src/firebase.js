cd608fa0c4d73b694d5cf310db5ddcb583603c87
Badal Mishra <39099105+Badalmishra@users.noreply.github.com>
2019-02-06 21:54:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 var config = {
-    apiKey: "AIzaSyCbWDXngCJLYh9oYj8fE7YCAmSKjWZ6w7g",
+    apiKey: "###############################",
     authDomain: "fire-react-todo.firebaseapp.com",
     databaseURL: "https://fire-react-todo.firebaseio.com",
     projectId: "fire-react-todo",
     storageBucket: "",
-    messagingSenderId: "99353783006"
+    messagingSenderId: "###################"
   };
 firebase.initializeApp(config);
 export const provider = new firebase.auth.GoogleAuthProvider();
